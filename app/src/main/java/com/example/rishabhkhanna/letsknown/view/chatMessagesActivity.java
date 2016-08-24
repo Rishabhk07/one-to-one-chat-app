6dadf271776d5a194132cee803d2f6787a4de7f4
Rishabh Khanna <rishabhkhanna777@gmail.com>
2016-08-25 01:28:44 +0530

    chat messages ui

---
+++ app/src/main/java/com/example/rishabhkhanna/letsknown/view/chatMessagesActivity.java
@@ -0,0 +1,13 @@
+package com.example.rishabhkhanna.letsknown.view;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class chatMessagesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chat_messages);
+    }
+}
